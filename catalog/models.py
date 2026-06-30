c75f99b8fa88b24520ab9db8a7c9b3ec280b3998
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-06-30 16:20:45 +0300

    correction_add_models models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title_name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.CharField(max_length=150, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title_name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title_name']
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=150, verbose_name='Название')
+    product_description = models.CharField(max_length=150, verbose_name='Описание')
+    picture = models.ImageField(upload_to='photos/', verbose_name="Изображение")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", verbose_name="Категория")
+    purchase_price = models.IntegerField(null=True, verbose_name="Цена за покупку")
+    created_ad = models.DateTimeField(auto_now_add=True)
+    updated_ad = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['product_name']
