b2c4fefe5dad1fd5ba2762200abf89a9c24f2208
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-07-04 03:39:02 +0300

    add five product in views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Product
 
 def home(request):
+    five_products = Product.objects.order_by('created_ad')[:5]
+    print(five_products)
     return render(request, 'catalog/home.html')
 
 def contacts(request):
