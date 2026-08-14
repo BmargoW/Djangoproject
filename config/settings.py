55902981450898971d2be9080285ca339672c938
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-08-15 02:00:57 +0300

    add send_mail

--- config/settings.py
+++ config/settings.py
@@ -142,3 +142,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = '/catalog/home_2/'
+
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- users/views.py
+++ users/views.py
@@ -1,15 +1,35 @@
+import os
+from dotenv import load_dotenv
 from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-from django.views.generic import FormView
-
+from django.core.mail import send_mail
 from .forms import UserRegisterForm
 from users.models import CustomUser
 
-class RegisterView(FormView):
+load_dotenv(override=True)
+
+class RegisterView(CreateView):
     model = CustomUser
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('catalogs:home_2')
+
+    def form_valid(self, form):
+        user = form.save()
+        self.send_welcome_mail(user.email)
+
+        return super().form_valid(form)
+
+    def send_welcome_mail(self, user_email):
+        subject = "Добро пожаловать!"
+        message = "Правильно сделали, что зарегистрировались!"
+        from_email = os.getenv("EMAIL_HOST_USER")
+        recipient_list = [user_email]
+        send_mail(subject, message, from_email, recipient_list)
+
+
+
 
 
 
